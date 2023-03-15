78a59b20450d96fd190cce4b9a9f35e2035c00f9
XCODEJET <117024606+xcodejet@users.noreply.github.com>
2023-03-15 10:11:37 +0530

    XCODEJET

---
+++ Kumuthu.js
@@ -0,0 +1,49 @@
+let image = [
+    {
+        image: "1",
+        title: "Abstract",
+        description: "Created by Dineth Indusara."
+    },
+    {
+        image: "2",
+        title: "Abstract 2",
+        description: "Created by Dineth Indusara."
+    },
+    {
+        image: "3",
+        title: "Flowers",
+        description: "Edited by Dineth Indusara."
+    },
+    {
+        image: "4",
+        title: "Butterfly",
+        description: "Created by Dineth Indusara."
+    }
+]
+
+const XCODEJETinject = (kumuthu) => {
+    var element = image[kumuthu]
+    element = `
+    <div class="box-border">
+        <div class="box">
+            <img src="./Upload/${element.image}.png" alt="">
+            <h1>${element.title}</h1>
+            <h3>${element.description}</h3>
+        </div>
+    </div>`
+    document.getElementById("gallery").innerHTML += element
+}
+
+var imageState = 0
+const Proccess = () => {
+    if(image[imageState])
+    {
+        XCODEJETinject(imageState)
+        imageState++
+    }
+    if(image[imageState])
+    {
+        Proccess()
+    }
+}
+Proccess()
